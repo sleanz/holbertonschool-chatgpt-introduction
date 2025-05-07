e1e369006a2c843f01482b9e42d44529738d3f73
sleanz <slean@sleanpc>
2025-05-07 15:01:41 +0200

    essaie 3

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import sys
+
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # This line was missing
+        n -= 1  # Cette ligne est essentielle pour décrémenter n à chaque itération
     return result
+
 f = factorial(int(sys.argv[1]))
 print(f)
